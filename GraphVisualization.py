3dac6207ebe4b8e884d3cade49ecad5a8d35ba37
Ishikawa <rashkinn27@gmail.com>
2024-07-20 01:34:04 +0300

    Added simple graph visualization with pyvis

---
+++ GraphVisualization.py
@@ -0,0 +1,14 @@
+from pyvis.network import Network
+#import networkx as nx
+
+net = Network('1000px', '1000px', bgcolor="#222222", font_color="white")
+
+net.add_node(1, label="Skill 1", title="Link to Skill 1 guide")
+net.add_node(2, label="Skill 2", title="Link to Skill 2 guide")
+net.add_node(3, label="Skill 3", title="Link to Skill 3 guide")
+
+net.add_edge(1, 3)
+
+net.barnes_hut()
+
+net.show("myquark.html", notebook=False)
